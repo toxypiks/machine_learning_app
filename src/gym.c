2d1b8c2e4a11cc93ca4cb91b1681f9420fc0c5bf
toxypiks <HannaHinkelbein89@gmx.de>
2024-12-28 21:17:19 +0100

    Visualize adder neural network with input/output data using raylib

--- src/gym.c
+++ src/gym.c
@@ -13,6 +13,8 @@
 #define IMG_WIDTH (16*IMG_FACTOR)
 #define IMG_HEIGHT (9*IMG_FACTOR)
 
+#define BITS 4
+
 //struct for macro for dynamic arrays
 
 typedef struct {
@@ -42,6 +44,47 @@ char *args_shift(int *argc, char ***argv)
   (*argc) -= 1;
   (*argv) += 1;
   return result;
+}
+
+void nn_render_raylib(NN nn)
+{
+  Color background_color = {0x18, 0x18, 0x18, 0xFF};
+  Color low_color = {0xFF, 0x00, 0xFF, 0xFF};
+  Color high_color = {0x00, 0xFF, 0x00, 0xFF};
+
+  ClearBackground(background_color);
+
+  int neuron_radius = 25;
+  int layer_border_vpad = 50;
+  int layer_border_hpad = 50;
+  int nn_width = IMG_WIDTH - 2*layer_border_hpad;
+  int nn_height = IMG_HEIGHT - 2*layer_border_vpad;
+  int nn_x = IMG_WIDTH/2 - nn_width/2;
+  int nn_y = IMG_HEIGHT/2 - nn_height/2;
+  size_t arch_count = nn.count + 1;
+  int layer_hpad = nn_width / arch_count;
+  for (size_t l = 0; l < arch_count; ++l) {
+    int layer_vpad1 = nn_height / nn.as[l].cols;
+    for (size_t i = 0; i < nn.as[l].cols; ++i) {
+      int cx1 = nn_x + l*layer_hpad + layer_hpad/2;
+      int cy1 = nn_y + i*layer_vpad1 + layer_vpad1/2;
+      if (l+1 < arch_count) {
+        int layer_vpad2 = nn_height / nn.as[l+1].cols;
+        for (size_t j = 0; j < nn.as[l+1].cols; ++j) {
+          int cx2 = nn_x + (l+1)*layer_hpad + layer_hpad/2;
+          int cy2 = nn_y + j*layer_vpad2 + layer_vpad2/2;
+          high_color.a = floorf(255.f*sigmoidf(MAT_AT(nn.ws[l], j, i)));
+          DrawLine(cx1, cy1, cx2, cy2, ColorAlphaBlend(low_color, high_color, WHITE));
+        }
+      }
+      if (l > 0) {
+        high_color.a = floorf(255.f*sigmoidf(MAT_AT(nn.bs[l-1], 0, i)));
+        DrawCircle(cx1, cy1, neuron_radius, ColorAlphaBlend(low_color, high_color, WHITE));
+      } else {
+        DrawCircle(cx1, cy1, neuron_radius, GRAY);
+      }
+    }
+  }
 }
 
 int main (int argc, char **argv)
@@ -112,22 +155,27 @@ int main (int argc, char **argv)
   MAT_PRINT(ti);
   MAT_PRINT(to);
 
-  /*InitWindow(IMG_WIDTH, IMG_HEIGHT, "gym");
+  size_t arch_nn[] = {2*BITS, 4*BITS, BITS + 1};
+  NN nn = nn_alloc(arch.items, arch.count);
+  NN g = nn_alloc(arch.items, arch.count);
+  nn_rand(nn, 0, 1);
+  NN_PRINT(nn);
+  float rate = 1;
+
+  InitWindow(IMG_WIDTH, IMG_HEIGHT, "gym");
   SetTargetFPS(60);
 
   size_t i = 0;
   while(!WindowShouldClose()) {
     if (i < 5000) {
       nn_backprop(nn, g, ti, to);
-      nn_apply_finite_diff(nn, g, rate);
+      nn_learn(nn, g, rate);
       i += 1;
       printf("c = %f\n", nn_cost(nn, ti, to));
     }
     BeginDrawing();
-    ClearBackground(RAYWHITE);
     nn_render_raylib(nn);
     EndDrawing();
-	}*/
-
+  }
   return 0;
 }
