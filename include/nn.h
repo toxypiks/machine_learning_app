6f50e4144835cb9bc824f0fc1152e58d2b4a247b
toxypiks <HannaHinkelbein89@gmx.de>
2025-01-01 14:13:01 +0100

    Implement function to randomize matrizes for stochastic gradient decent

--- include/nn.h
+++ include/nn.h
@@ -49,6 +49,7 @@ void mat_dot(Mat dst, Mat a, Mat b);
 void mat_sum(Mat dst, Mat a);
 void mat_sig(Mat m);
 void mat_print(Mat m, const char* name, size_t padding);
+void mat_shuffle_rows(Mat m);
 #define MAT_PRINT(m) mat_print(m, #m, 0)
 
 typedef struct {
@@ -184,6 +185,19 @@ void mat_sig(Mat m) {
 	for (size_t j = 0; j < m.cols; ++j) {
 	  MAT_AT(m, i, j) = sigmoidf(MAT_AT(m, i, j));
 	}
+  }
+}
+
+//Fisher-Yates shuffle
+void mat_shuffle_rows(Mat m)
+{
+  for (size_t i = 0; i < m.rows; ++i) {
+    size_t j = i + rand()%(m.rows - i);
+    for (size_t k = 0; k < m.cols; ++k) {
+      float t = MAT_AT(m, i, k);
+      MAT_AT(m, i, k) = MAT_AT(m, j, k);
+      MAT_AT(m, j, k) = t;
+    }
   }
 }
 
--- src/png2mat.c
+++ src/png2mat.c
@@ -163,6 +163,10 @@ int main (int argc, char **argv)
     }
   }
 
+  mat_shuffle_rows(t);
+  MAT_PRINT(t);
+  return 0;
+
   Mat ti = {
     .rows = t.rows,
     //we defined two inputs
@@ -236,6 +240,7 @@ int main (int argc, char **argv)
       nn_render_raylib(nn, rx, ry, rw, rh);
 
       rx += rw;
+      float scale = 15.0f;
       for (size_t y = 0; y < (size_t)img_height; ++y) {
         for (size_t x = 0; x < (size_t)img_width; ++x) {
           size_t i = y*img_width + x;
@@ -248,7 +253,7 @@ int main (int argc, char **argv)
       }
 
       UpdateTexture(preview_texture, preview_image.data);
-      DrawTextureEx(preview_texture, CLITERAL(Vector2) {rx, ry}, 0, 15, WHITE);
+      DrawTextureEx(preview_texture, CLITERAL(Vector2) {rx, ry}, 0, scale, WHITE);
 
       char buffer[256];
       snprintf(buffer, sizeof(buffer),"Epoch: %zu/%zu, Rate: %f", epoch, max_epoch, rate);
